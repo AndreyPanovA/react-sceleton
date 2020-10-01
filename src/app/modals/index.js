61c366dfc41c7aa8a4bbb7fd7a5acc690aeacd0d
AndreyPanovA <a.andrey.panov@gmail.com>
2020-10-01 12:15:22 +0300

    start

---
+++ src/app/modals/index.js
@@ -0,0 +1,37 @@
+import React, { useCallback } from 'react';
+import modal from '@store/modal/actions';
+import * as modals from './config.js';
+import useSelectorMap from '@utils/hooks/use-selector-map';
+
+function Modals() {
+  const select = useSelectorMap(state => ({
+    modal: state.modal,
+  }));
+
+  const callbacks = {
+    /**
+     * Выбор компонента окна, если нужно показывать
+     * @returns {null|*}
+     */
+    getModal: useCallback(() => {
+      const props = {
+        ...select.modal.params,
+        close: result => {
+          return modal.close(result);
+        },
+      };
+      if (select.modal.show) {
+        if (modals[select.modal.name]) {
+          const Component = modals[select.modal.name];
+          return <Component {...props} />;
+        }
+      } else {
+        return null;
+      }
+    }, [select]),
+  };
+
+  return callbacks.getModal();
+}
+
+export default React.memo(Modals);
---
+++ src/components/layouts/layout-page/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classes from './style.less';
+import themes from '@utils/themes';
+import cn from 'classnames';
+
+function LayoutPage (props) {
+  return (
+    <div className={themes('LayoutPage', props.theme)}>
+      <div className="LayoutPage__header">{props.header}</div>
+      <div className="LayoutPage__content">{props.children || props.content}</div>
+      <div className="LayoutPage__footer">{props.footer}</div>
+    </div>
+  );
+}
+
+LayoutPage.propTypes = {
+  header: PropTypes.node,
+  content: PropTypes.node,
+  footer: PropTypes.node,
+  children: PropTypes.node,
+  theme: PropTypes.oneOfType([PropTypes.string, PropTypes.array]), // можно передать несколько тем через пробел или массивом
+};
+
+LayoutPage.defaultProps = {
+  theme: ''
+}
+
+export default React.memo(LayoutPage);
